b4a2951ecd2f8f6aad2f670242b00d32e0fcbd06
Guilherme Malafaia de Sousa Silva <124741164+Malafaiaa@users.noreply.github.com>
2024-06-24 03:03:26 -0300

    Update service.ts

--- src/app/api/service.ts
+++ src/app/api/service.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 
-const BASE_PATH = "http://localhost:3000";
+const BASE_PATH = process.env.BASE_PATH || "https://tcc-etec-paulistano-6whgfb0w6.vercel.app"; // Usando a variável de ambiente ou localhost por padrão
 
 export async function postCriarPix(body: any) {
-    return await axios.post(`${BASE_PATH}/api/payment/pix`,
-        { body }
-    )
+    try {
+        const response = await axios.post(`${BASE_PATH}/api/payment/pix`, body);
+        return response.data;
+    } catch (error) {
+        console.error("Erro ao fazer requisição:", error);
+        throw error;
+    }
 }
