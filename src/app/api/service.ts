3d0b45d3195a014d52b4d3393443a0f6dc6c93ff
Guilherme Malafaia de Sousa Silva <124741164+Malafaiaa@users.noreply.github.com>
2024-06-24 02:31:28 -0300

    Update service.ts

--- src/app/api/service.ts
+++ src/app/api/service.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_PATH = "https://tcc-etec-paulistano-k9m7korcf.vercel.app";
+const BASE_PATH = "http://localhost:3000";
 
 export async function postCriarPix(body: any) {
     return await axios.post(`${BASE_PATH}/api/payment/pix`,
